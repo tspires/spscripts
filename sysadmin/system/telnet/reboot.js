67c8f4701e87eb30b04e080bc70d13423e6c1324
Tim Spires <timothy.s.spires@gmail.com>
2015-06-26 17:19:50 -0600

    Added reboot telnet script

---
+++ sysadmin/system/telnet/reboot.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+var telnet   = require('telnet-client');
+var log      = require('npmlog');
+
+var connection = new telnet();
+
+var params = {
+  host : '192.168.0.13',
+  timeout: 1500
+};
+
+var cmd = 'ls';
+connection.on('ready', function(prompt) {
+  connection.exec(cmd, function(response) {
+    log.info(response); 
+  });
+});
+
+connection.on('timeout', function() {
+  log.warn('Socket timeout...');
+  connection.end();
+});
+
+connection.on('close', function() {
+  log.info('Connection closed...');
+});
+
+connection.connect(params);
